9af4a00664d5c63887b1792454a808b6750c42cf
Mahcks <maxnatural13@gmail.com>
2022-10-14 20:02:24 -0500

    Changed default message

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,8 +2,7 @@ const Home = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <h1 className="text-3xl font-bold text-blue-600">
-        Install & Setup Vite + React + Typescript + Tailwind CSS 3 + React
-        Router DOM
+        Vite + React + Typescript + Tailwind CSS 3 + React Router DOM
       </h1>
     </div>
   )
